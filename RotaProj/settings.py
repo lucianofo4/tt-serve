9baebd0270dcaa5f1645e9f7dee5febc69b1773e
lucianofo4 <56547033+lucianofo4@users.noreply.github.com>
2022-07-15 10:14:37 -0300

    mudar heroku set

--- RotaProj/settings.py
+++ RotaProj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,4 @@ LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'initial_page'
 
 # LOGOUT_REDIRECT_URL = 'login'
+django_heroku.settings(locals())
